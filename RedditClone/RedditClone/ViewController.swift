252ca765e62f70665d459b9eb7d33d3b82307203
Nadine Siregar <anbita.siregar@go-jek.com>
2017-07-24 23:53:00 +0700

    [Nadine] Added SubmitTopicTableViewCell to tableView

--- RedditClone/RedditClone/ViewController.swift
+++ RedditClone/RedditClone/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, TopicTableViewCellDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, TopicTableViewCellDelegate, SubmitTopicTableViewCellDelegate {
     
     @IBOutlet weak var topicsTableView: UITableView?
     
@@ -18,6 +18,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
 
         topicsTableView?.register(UINib(nibName: "TopicTableViewCell", bundle: nil), forCellReuseIdentifier: "TopicTableViewCell")
+        topicsTableView?.register(UINib(nibName: "SubmitTopicTableViewCell", bundle: nil), forCellReuseIdentifier: "SubmitTopicTableViewCell")
         
         // For testing purposes only
         for topicNumber in 1...20 {
@@ -27,15 +28,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return topicsList.count
+        return topicsList.count + 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "SubmitTopicTableViewCell") as? SubmitTopicTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            cell.delegate = self
+            
+            return cell
+        }
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TopicTableViewCell") as? TopicTableViewCell else {
             return UITableViewCell()
         }
         
-        let cellTopic: Topic = topicsList[indexPath.row]
+        let cellTopic: Topic = topicsList[indexPath.row - 1]
 
         cell.topic = cellTopic
         cell.delegate = self
@@ -49,6 +60,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         topicsList.sort { (topic1, topic2) -> Bool in
             return topic1.count > topic2.count
         }
+        topicsTableView?.reloadData()
+    }
+    
+    // SubmitTopicTableViewCellDelegate
+    func addNewTopic(topic: String?) {
+        guard let topic = topic else {
+            return
+        }
+        
+        let newTopic: Topic = Topic(topicName: topic, count: 0)
+        topicsList.append(newTopic)
         topicsTableView?.reloadData()
     }
 }
