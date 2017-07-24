d4ebfd47c956801dd536a99c00596266ab4174f5
Nadine Siregar <anbita.siregar@go-jek.com>
2017-07-25 02:20:37 +0700

    [Nadine] Added unit tests for business logic in ViewController

--- RedditClone/RedditCloneTests/RedditCloneTests.swift
+++ RedditClone/RedditCloneTests/RedditCloneTests.swift
@@ -11,9 +11,13 @@ import XCTest
 
 class RedditCloneTests: XCTestCase {
     
+    var viewController: ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        viewController = storyboard.instantiateInitialViewController() as! ViewController
+        viewController.topicsList = []
     }
     
     override func tearDown() {
@@ -21,16 +25,55 @@ class RedditCloneTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTopicSort() {
+        var topicsList: [Topic] = []
+        topicsList.append(Topic(topicName: "Topic1", count: 2))
+        topicsList.append(Topic(topicName: "Topic2", count: 1))
+        topicsList.append(Topic(topicName: "Topic3", count: 10))
+        
+        viewController.topicsList = topicsList
+        viewController.refreshTableView()
+        
+        // refreshTableView() sorts the topics list by descending order of count, so the order of topicsList after this call should
+        // Topic3 -> Topic1 -> Topic2
+        XCTAssertEqual(viewController.topicsList[0].topicName, "Topic3")
+        XCTAssertEqual(viewController.topicsList[1].topicName, "Topic1")
+        XCTAssertEqual(viewController.topicsList[2].topicName, "Topic2")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAddTopicErrorWithEmptyTopic() {
+        // Here, we start with an empty topicsList
+        
+        // We try adding a new EMPTY topic
+        viewController.addNewTopic(topic: "")
+        
+        // After adding empty topic, topicsList shouldn't have changed because this is invalid input
+        XCTAssertEqual(viewController.topicsList.count, 0)
+    }
+    
+    func testAddTopicErrorWithFullTopicsList() {
+        
+        // We start with a full topicsList. Max number of topics is 20
+        for topicCount in 1...20 {
+            let newTopic: Topic = Topic(topicName: "Topic \(topicCount)", count: 1)
+            viewController.topicsList.append(newTopic)
         }
+        
+        // We try adding topic 21
+        viewController.addNewTopic(topic: "Topic 21")
+        
+        // TopicsList shouldn't have changed because we cannot add anymore topics after max number (20) is reached
+        XCTAssertEqual(viewController.topicsList.count, 20)
+    }
+    
+    func testAddTopicSuccess() {
+        // Here, we start with an empty topicsList
+        
+        // We try adding topic 1
+        viewController.addNewTopic(topic: "Topic 1")
+        
+        // Topic1 should have been added to TopicsList
+        XCTAssertEqual(viewController.topicsList.count, 1)
     }
     
 }
