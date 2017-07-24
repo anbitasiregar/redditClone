55366fda0fc8a92fb78ad7e08c7e2216d9062996
Nadine Siregar <anbita.siregar@go-jek.com>
2017-07-24 23:15:37 +0700

    [Nadine] Added Topic data model

---
+++ RedditClone/RedditClone/Models/Topic.swift
@@ -0,0 +1,19 @@
+//
+//  Topic.swift
+//  RedditClone
+//
+//  Created by Nadine Siregar on 7/24/17.
+//  Copyright © 2017 Nadine Siregar. All rights reserved.
+//
+
+import Foundation
+
+class Topic {
+    var topicName: String
+    var count: Int
+    
+    init(topicName: String, count: Int) {
+        self.topicName = topicName
+        self.count = count
+    }
+}
