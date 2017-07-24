ed13a45485f5ee6e5278b1f5cb71642354087d80
Nadine Siregar <anbita.siregar@go-jek.com>
2017-07-24 22:41:37 +0700

    [Nadine] Added TableView with TopicTableViewCell

--- RedditClone/RedditClone/ViewController.swift
+++ RedditClone/RedditClone/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var topicsTableView: UITableView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        topicsTableView?.register(UINib(nibName: "TopicTableViewCell", bundle: nil), forCellReuseIdentifier: "TopicTableViewCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TopicTableViewCell") else {
+            return UITableViewCell()
+        }
+        
+        return cell
     }
-
-
 }
 
